98d7345224c35880b14ad1d284468a482988b995
nguyen dl - home <congnguyen008@gmail.com>
2020-05-24 22:56:22 +0700

    update

---
+++ sdk/type.go
@@ -0,0 +1,38 @@
+package sdk
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
+type Array []interface{}
+
+//Value json Marshal to byte
+func (a Array) Value() (driver.Value, error) {
+	bytes, err := json.Marshal(a)
+	return string(bytes), err
+}
+
+func (a Array) ArrayString() ([]string, error) {
+	var arr []string
+	bytes, err := json.Marshal(a)
+	err = json.Unmarshal(bytes, &arr)
+	if err != nil {
+		return nil, err
+	}
+	return arr, nil
+
+}
+
+//Scan string or byte Unmarshal to json
+func (a *Array) Scan(src interface{}) error {
+	switch value := src.(type) {
+	case string:
+		return json.Unmarshal([]byte(value), a)
+	case []byte:
+		return json.Unmarshal(value, a)
+	default:
+		return errors.New("Not support!")
+	}
+}
