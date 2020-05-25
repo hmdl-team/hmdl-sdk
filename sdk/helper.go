3cb3be7811a879cc8f7d5650fd25d1f4c5fa6bde
Cong nguyen - work <congnguyen008@gmail.com>
2020-05-25 14:48:10 +0700

    update

--- sdk/helper.go
+++ sdk/helper.go
@@ -34,3 +34,70 @@ func MaHoa(data string) string {
 
 	return sha
 }
+
+func Escape(source string) string {
+	var j int = 0
+	if len(source) == 0 {
+		return ""
+	}
+	tempStr := source[:]
+
+	desc := make([]byte, len(tempStr)*2)
+	for i := 0; i < len(tempStr); i++ {
+		flag := false
+		var escape byte
+		switch tempStr[i] {
+		case '\r':
+			flag = true
+			escape = '\r'
+			break
+		case '\n':
+			flag = true
+			escape = '\n'
+			break
+		case '\\':
+			flag = true
+			escape = '\\'
+			break
+		case '\'':
+			flag = true
+			escape = '\''
+			break
+		case '"':
+			flag = true
+			escape = '"'
+			break
+		case '\032':
+			flag = true
+			escape = 'Z'
+			break
+		default:
+		}
+		if flag {
+			desc[j] = '\\'
+			desc[j+1] = escape
+			j = j + 2
+		} else {
+			desc[j] = tempStr[i]
+			j = j + 1
+		}
+	}
+	return string(desc[0:j])
+
+}
+
+func TaoChuoi(dsId []int) string {
+
+	inCondition := ""
+	for _, item := range dsId {
+		if inCondition != "" {
+			inCondition += ", "
+		}
+		if item > 0 {
+			inCondition += strconv.Itoa(item)
+		}
+	}
+
+	return inCondition
+
+}
